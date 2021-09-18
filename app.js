f0949e37c900faf63c30e9dcd98dd8315a022a77
DORi <66077606+baedori3@users.noreply.github.com>
2021-09-18 23:51:07 +0900

    submit form을 통해 입력한 이름이 나오도록 수정

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginBtn = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick() {
-    console.log(loginInput.value);
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+    event.preventDefault();  
+    // 브라우저의 기본 동작 막기 (브라우저가 submit할 때 원래 하는 동작을 안하게 됨)
+    loginForm.classList.add("hidden");
+    const username = loginInput.value;
+    greeting.innerText = `안녕하세요 ${username}님`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginBtn.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+
